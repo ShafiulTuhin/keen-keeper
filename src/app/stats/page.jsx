9d9902c9d8ff0aa8f947fbea0e0bb68d010b9ceb
ShafiulTuhin <tuhin6488@gmail.com>
2026-04-13 19:24:23 +0600

    completed Navbar and footer design

---
+++ src/app/stats/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>This is stats page</div>;
+};
+
+export default page;
---
+++ src/app/timeline/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Time line page</div>;
+};
+
+export default page;
